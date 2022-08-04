ef31992a9e295b7eedcc63dd8cee5838f5c486b5
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-04 00:00:43 -0400

    created emaployee insert for seed sql file

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -14,6 +14,16 @@ VALUES ("Sales Lead , 100000, 1")
        ("Legal Team Leader, 250000, 4")
        ("Lawyer, 190000, 4")
 
-INSERT INTO employee ()
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John, Doe, Sales Lead, 1")
+       ("Mike, Chan, Saleperson, 2")
+       ("Ashley, Rodriguez, Lead Engineer, 3")
+       ("Kevin, Tupik, Software Enigneer, 4")
+       ("Kunal, Singh, Account Manager, 5")
+       ("Malia, Brown, Accountant, 6 ")
+       ("Sarah, Lourd, Legal Team Lead, 7")
+       ("Tom, Allen, Lawyer, 8")
+
+
 
 
