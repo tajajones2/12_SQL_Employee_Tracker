a5fcaa295ff39de062d600860f687ff484474215
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-03 23:44:47 -0400

    added department && role insrts on seeds sql file

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department ()
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead , 100000, 1")
+       ("Salesperon , 80000, 1")
+       ("Lead Engineer, 150000, 2")
+       ("Software Engineer, 120000, 2")
+       ("Account Manager, 160000, 3")
+       ("Accountant, 125000, 3")
+       ("Legal Team Leader, 250000, 4")
+       ("Lawyer, 190000, 4")
+
+INSERT INTO employee ()
+
+
