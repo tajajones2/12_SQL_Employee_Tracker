fce8e19d55b72bdc7372022d24539d3b95c151ea
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-04 01:07:03 -0400

    fixed error on schema sql file

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,29 +5,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department(
-    id INT NO NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 
 );
 
-CREATE TABLE ROLE (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     title VARCHAR (30) 
-     salary DECIMAL (10,2)
+     title VARCHAR (30),
+     salary DECIMAL,
      department_id int,
-    FOREIGN KEY (department_id) REFERENCES department (id) 
-    ON DELETE CASCADE
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name: VARCHAR(30),
+    first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id), 
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 
-    FOREIGN KEY (role_id) REFERENCES role () ON DELETE CASCADE
-    FOREIGN key (manager_id) REFERENCES employee (id) ON DELETE SET NULL
-
-)
+);
