bf1e71f6a369095179c492365d8a2beeefe5386f
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-03 23:24:18 -0400

    created employee table role on schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -16,5 +16,18 @@ CREATE TABLE ROLE (
      salary DECIMAL (10,2)
      department_id int,
     FOREIGN KEY (department_id) REFERENCES department (id) 
+    ON DELETE CASCADE
 
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name: VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+
+    FOREIGN KEY (role_id) REFERENCES role () ON DELETE CASCADE
+    FOREIGN key (manager_id) REFERENCES employee (id) ON DELETE SET NULL
+
+)
