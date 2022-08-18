ffb07996c4bbc3f203aecfc92d4eaa9fc73a2419
Taja Jones <102881062+tajajones2@users.noreply.github.com>
2022-08-17 21:26:16 -0400

    added commas to select section of queries sql and created

--- Develop/db/queries.sql
+++ Develop/db/queries.sql
@@ -7,8 +7,12 @@ employee.id,
 employee.first_name, 
 employee.last_name,
 role.title,
-department.name
-role.salary
+department.name,
+role.salary,
 CONCAT(mgr.first_name, " " , mgr.last_name) as manager
 FROM employee
 LEFT JOIN role ON role.id= employee.role_id
+LEFT JOIN department ON role.department_id=department.id 
+LEFT JOIN employee as mrg on employee.id = mrg.manager_id
+
+
